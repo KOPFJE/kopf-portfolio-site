14c0242ff7b5273288a9ac33e237fe6695b093ea
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-03-12 17:06:10 +0200

    Finalized mongoDB connection.

--- server/server.js
+++ server/server.js
@@ -2,7 +2,17 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env"});
 const port = process.env.PORT || 5000;
+const dbo = require("./db/conn");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.listen(port, () => {
+    // perform a database connection when server starts
+    dbo.connectToServer(function (err) {
+      if (err) console.error(err);
+  
+    });
+    console.log(`Server is running on port: ${port}`);
+  });
