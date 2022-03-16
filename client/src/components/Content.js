42f355abc430e81d8223f44c9be9f555d0d16399
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-03-16 03:48:21 +0200

    Added all the static data.

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -10,7 +10,8 @@ function Content(props) {
             imglink : "/showcaseimgs/portfolio_example.png",
             infolink : "https://github.com/KOPFJE/kopf-portfolio-site/",
             content : 
-            `This portfolio right now is still work in progress. As such, the public deployment is currently just a static page. The finished product will use the [MERN stack](https://www.mongodb.com/mern-stack) using the architecture of React as front-end, Express as back-end and an API between the front and back. The content is stylized using the React-markdown plugin. The finished product will have an in-app authentication for an admin dashboard, where an admin can add, remove and modify portfolio showcase posts and modify the info of the portfolio user.`
+            `This portfolio right now is still work in progress. As such, the public deployment is currently just a static page. The finished product will use the [MERN stack](https://www.mongodb.com/mern-stack) using the architecture of React as front-end, Express as back-end and an API between the front and back. 
+            The content is stylized using the React-markdown plugin. The finished product will have an in-app authentication for an admin dashboard, where an admin can add, remove and modify portfolio showcase posts and modify the info of the portfolio user.`
         },
         {
             title : "TJTS5901 - Digicanem Dogmap",
@@ -20,37 +21,38 @@ function Content(props) {
             imglink : "/showcaseimgs/digicanem_example.png",
             infolink : "https://gitlab.jyu.fi/digicanem/tjts5901-dogmap_template",
             content : 
-            `This project was done with a group of 4 for the course "Continuous Software Engineering" (TJTS5901). I mainly focused on handling the pipeline for the project, but I also implemented some of the features like marker handling. I also made a lot of the UI design and implementation.`
+            `This project was done with a group of 4 for the course "Continuous Software Engineering" (TJTS5901). The project is a web app for users to mark out good dogwalking spots in the map. I mainly focused on handling the pipeline for the project, but I also implemented some of the features like marker handling. I also made a lot of the UI design and implementation.
+            The project was done in an agile way using a sprint structure. There also was a client for the project, [Discovery Street](https://discoverystreet.fi/), making the course a very unique during my time in the University of Jyväskylä.`
         },
         {
             title : "Conthrive 2020 - Alvacha",
             teamsize : 5,
             languagestack : ["JavaScript", "HTML", "CSS"],
             techstack : ["Chart.js", "Materialize UI"],
-            imglink : "/showcaseimgs/portfolio_example.png",
+            imglink : "/showcaseimgs/alvacha_example.png",
             infolink : "https://github.com/TSkoll/Alvacha",
             content : 
-            "**This** is a *test*. [Here](https://github.com/KOPFJE/kopf-portfolio-site/) is the GitHub repository for this project."
+            `Alvacha was the result of a competition for the hackathon [Conthrive 2020](https://www.facebook.com/conthrive/), where our group of friends (me and 4 students from Jyväskylä University of Applied Sciences) developed a prototype for parsing and plotting water usage data for analysis using JavaScript and Chart.js framework. This event helped me work in a group under a tight schedule.`
         },
         {
-            title : "Own portfolio site",
-            teamsize : 1,
-            languagestack : ["JavaScript", "HTML", "CSS"],
-            techstack : ["React.js", "MongoDB", "Node.js"],
-            imglink : "/showcaseimgs/portfolio_example.png",
-            infolink : "https://github.com/KOPFJE/kopf-portfolio-site/",
+            title : "HTKA114 - Project Nauternius",
+            teamsize : 5,
+            languagestack : ["C#"],
+            techstack : ["Unity"],
+            imglink : "/showcaseimgs/nauternius_example.png",
+            infolink : "https://github.com/MoleTrooper/nauternius",
             content : 
-            "**This** is a *test*. [Here](https://github.com/KOPFJE/kopf-portfolio-site/) is the GitHub repository for this project."
+            `Project Nauternius was a prototype developed for the game development project course HTKA114. The course was interdisciplinary with students coming from all different faculties from JYU. I worked as a game designer and a programmer. Programming side my main focus was the camera. Designing wise I was a part of handling the puzzle and level design.`
         },
         {
-            title : "Own portfolio site",
+            title : "TIEP111 - Ice hockey team registry",
             teamsize : 1,
-            languagestack : ["JavaScript", "HTML", "CSS"],
-            techstack : ["React.js", "MongoDB", "Node.js"],
-            imglink : "/showcaseimgs/portfolio_example.png",
-            infolink : "https://github.com/KOPFJE/kopf-portfolio-site/",
+            languagestack : ["Java"],
+            techstack : ["JavaFX"],
+            imglink : "/showcaseimgs/ohj2_example.png",
+            infolink : "https://gitlab.jyu.fi/lajavani/ohj2",
             content : 
-            "**This** is a *test*. [Here](https://github.com/KOPFJE/kopf-portfolio-site/) is the GitHub repository for this project."
+            `The programming 2 course in JYU has a *legendary* status in the IT student organization. My version of the programming 2 course project is a program, which handles the management of an ice hockey team. The team has players, who can have injuries. The team also has matches. The program is done using the OOP paradigm.`
         }
     ];
 
