7f557c122aaf4274e73add3397ebe0b068942152
Lauri Niskanen <kopf.niskanen@gmail.com>
2022-03-16 03:56:36 +0200

    Deploy react app to GitHub Pages.

--- client/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer(props) {
-    return(
-        <footer id="mainfooter">
-            Lauri Niskanen 2022 - Made with <a href="https://reactjs.org/">React.js</a> - Source code at <a href="https://github.com/KOPFJE/kopf-portfolio-site">Github</a>
-        </footer>
-    );
-  }
-  export default Footer;
